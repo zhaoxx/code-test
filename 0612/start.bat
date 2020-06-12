1e83a5081583a592f4380df3a6eb0fae083b6d8d
zhaoxianxing <zhaoxianxing@mia.com>
2020-06-12 17:41:27 +0800

    删除多余文件

--- 0612/start.bat
+++ 0612/start.bat
@@ -1,4 +1,5 @@
 del *.class /s
 dir /s /b *.java >source.txt
 javac -encoding UTF-8 @source.txt
+del source.txt
 java MyCalendarTester
--- 0612/start.sh
+++ 0612/start.sh
@@ -1,4 +1,5 @@
 rm -rf *.class
 find -name "*.java" > source.txt
 javac -encoding UTF-8 @source.txt
+rm -rf source.txt
 java MyCalendarTester
